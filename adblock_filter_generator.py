b822a174726348929d177672d7f7cc6e947d13d8
ghostnetic <github@smellyshiba.com>
2023-04-14 10:43:00 -0400

    Update adblock_filter_generator.py

--- adblock_filter_generator.py
+++ adblock_filter_generator.py
@@ -51,7 +51,7 @@ def main():
     blocklist_urls = [
         'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/multi.txt',
         'https://gitlab.com/quidsup/notrack-blocklists/-/raw/master/trackers.hosts',
-        'https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt',
+        'https://hblock.molinero.dev/hosts_adblock.txt',
         'https://github.com/sjhgvr/oisd/raw/main/abp_small.txt'
     ]
 
