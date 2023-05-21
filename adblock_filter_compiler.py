6a2641dd5df9f3e1eff46b9d8ca24a948f9b0d47
Glitch <iGlitch@users.noreply.github.com>
2023-05-21 12:17:49 +0000

    Add Whitelist Feature
    Outputs to whitelist.txt

--- adblock_filter_compiler.py
+++ adblock_filter_compiler.py
@@ -21,11 +21,11 @@ def parse_hosts_file(content):
 
     return adblock_rules
 
-def generate_filter(file_contents):
+def generate_filter(blocklist_contents, whitelist_urls):
     duplicates_removed = 0
     adblock_rules_set = set()
 
-    for content in file_contents:
+    for content in blocklist_contents:
         adblock_rules = parse_hosts_file(content)
         for rule in adblock_rules:
             if rule not in adblock_rules_set:
@@ -35,35 +35,62 @@ def generate_filter(file_contents):
 
     sorted_rules = sorted(list(adblock_rules_set))
     header = generate_header(len(sorted_rules), duplicates_removed)
-    filter_content = '\n'.join([header, '', *sorted_rules]) # Added empty line after the header
-    return filter_content, duplicates_removed
+    filter_content = '\n'.join([header, '', *sorted_rules])  # Added empty line after the header
+
+    whitelist_rules = []
+    for url in whitelist_urls:
+        with requests.get(url) as response:
+            whitelist_content = response.text
+            whitelist_rules.extend(whitelist_content.splitlines())
+
+    whitelist_header = generate_whitelist_header(len(whitelist_rules))
+    whitelist_content = '\n'.join([whitelist_header, '', *whitelist_rules])
+
+    return filter_content, duplicates_removed, whitelist_content
 
 def generate_header(domain_count, duplicates_removed):
-    date = datetime.now().strftime('%Y-%m-%d')
     return f"""# Title: AdBlock Filter Compiler
 # Description: Python-based script that generates AdBlock syntax filters by combining and processing multiple blocklists, host files, and domain lists.
-# Created: {date}
+# Created: {datetime.now().strftime('%Y-%m-%d')}
 # Domain Count: {domain_count}
 # Duplicates Removed: {duplicates_removed}
 #==============================================================="""
 
+def generate_whitelist_header(domain_count):
+    return f"""# Title: AdBlock Whitelist Compiler
+# Description: List of whitelisted domains for AdBlock filters.
+# Created: {datetime.now().strftime('%Y-%m-%d')}
+# Domain Count: {domain_count}
+#==============================================================="""
+
 def main():
     blocklist_urls = [
+        # Hagezi Normal
         'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/multi.txt',
+        # Notrack
         'https://gitlab.com/quidsup/notrack-blocklists/-/raw/master/trackers.hosts',
+        # AdGuard
         'https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt',
     ]
 
-    file_contents = []
+    whitelist_urls = [
+        'https://raw.githubusercontent.com/AhaDNS/Aha.Dns.Domains/master/Domains/whitelist.txt',
+        # anudeep Whitelist
+        'https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt',
+    ]
+
+    blocklist_contents = []
     for url in blocklist_urls:
         with requests.get(url) as response:
-            file_contents.append(response.text)
+            blocklist_contents.append(response.text)
 
-    filter_content, duplicates_removed = generate_filter(file_contents)
+    filter_content, duplicates_removed, whitelist_content = generate_filter(blocklist_contents, whitelist_urls)
 
     with open('blocklist.txt', 'w') as f:
         f.write(filter_content)
 
+    with open('whitelist.txt', 'w') as f:
+        f.write(whitelist_content)
+
 if __name__ == "__main__":
     main()
-    
